ced72246a1d58e2d450d3de07c95ef268ecd67ca
eatwanderexplore <111604338+eatwanderexplore@users.noreply.github.com>
2025-02-16 14:02:50 +1300

    Updates

--- src/components/footer.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-// Footer Component
-function Footer() {
-    return <footer className="bg-blue-700 text-white text-center py-4">© {new Date().getFullYear()} Lighthouse Counseling</footer>;
-  }
-
-export default Footer;
